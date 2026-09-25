5280c5a95d80722075b26524560a438c49973357
Marcelo Salvador <mdsaiscientist@gmail.com>
2026-09-25 15:40:20 -0300

    Add normalized research result model

--- domain/research_result.py
+++ domain/research_result.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import Any
 # This class will give downstream components context
 # about what kind of result they received
+# Common result contract
 from domain.research_task import ResearchTaskType
 
 @dataclass
